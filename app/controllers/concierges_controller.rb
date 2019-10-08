8635168813fea4c96def628995839a5911aeb14a
Renee Nordholm <reneenordholm@gmail.com>
2019-10-08 17:57:45 -0500

    root setup to welcome#home

--- app/controllers/concierges_controller.rb
+++ app/controllers/concierges_controller.rb
@@ -1,2 +1,3 @@
 class ConciergesController < ApplicationController
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :clients
   resources :stylists
   resources :concierges
+
+  root 'welcome#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
