fef7ce92a9323ac3904f3d408b58df0cc99f9851
Renee Nordholm <reneenordholm@gmail.com>
2019-10-09 13:44:44 -0500

    concierge new/create setup

--- app/controllers/concierges_controller.rb
+++ app/controllers/concierges_controller.rb
@@ -1,3 +1,19 @@
 class ConciergesController < ApplicationController
 
+    def new
+        @concierge = Concierge.new
+    end
+
+    def create
+        @concierge = Concierge.create(concierge_params)
+        session[:concierge_id] = @concierge.id
+
+        redirect_to root_path
+    end
+    
+    private
+
+    def concierge_params
+        params.require(:concierge).permit(:username, :password, :email, :name, :admin)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
 
     def create
         @concierge = Concierge.find_by(username: params[:concierge][:username])
-
-        if !@concierge.nil? && @concierge.authenticate(params[:concierge][:password])
+   
+        if  @concierge.authenticate(params[:concierge][:password])
             session[:concierge_id] = @concierge.id
             redirect_to root_path
         else
+            binding.pry
             redirect_to login_path
         end
     end
