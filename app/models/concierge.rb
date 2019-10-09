6134e6710e1df890cffe652de0d28067c4fca516
Renee Nordholm <reneenordholm@gmail.com>
2019-10-09 15:56:25 -0500

    add validations to concierge model

--- app/models/concierge.rb
+++ app/models/concierge.rb
@@ -5,4 +5,8 @@ class Concierge < ApplicationRecord
     has_many :services, through: :appointments
 
     has_secure_password
+
+    validates :username, presence: true, uniqueness: true, length: { minimum: 4 }, format: { with: /\A[a-zA-Z]+\z/, message: "cannot contain blank space, numbers, or special characters" }
+    validates :email, presence: true, format: { with: /[^@]+@[^\.]+\..+/, message: "must be in format xxxx@xxxx.xxx"}
+    validates :password, presence: true, length: { in: 6..20 }
 end
