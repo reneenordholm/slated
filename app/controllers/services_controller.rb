24d9d51dcdf2e4cac3642846e010af65e15d1a85
Renee Nordholm <reneenordholm@gmail.com>
2019-10-09 15:45:03 -0500

    can view all appointments by client index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,8 @@
 class ServicesController < ApplicationController
+
+    def index
+        @services = Service.all
+    end
     
     def show
         @service = Service.find(params[:id])
--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,5 +1,10 @@
 class StylistsController < ApplicationController
 
+    def index
+        @stylists = Stylist.all
+    end
+
+
     def show
         @stylist = Stylist.find(params[:id])
     end
